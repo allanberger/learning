745f49ec514bfbd1aeaa84ffa70923db634ed413
Thomas Scheiblauer <tom@sharkbay.at>
2016-12-15 16:25:36 +0100

    reduce redundancy and use custom comparator
    should also be faster

--- clojure/adventofcode/src/adventofcode/days/day4.clj
+++ clojure/adventofcode/src/adventofcode/days/day4.clj
@@ -2,33 +2,28 @@
   (:require [clojure.string :as s]
             [clojure.java.io :as io]))
 
-(defn room-name [room]
-  (subs room 0 (- (count room) 11)))
-
-(defn sector-id [room]
-  (let [length (count room)]
-    (subs room (- length 10) (- length 7))))
-
-(defn checksum [room]
-  (let [length (count room)]
-    (subs room (- length 6) (- length 1))))
+(defn compare-freq [[k1 v1] [k2 v2]]
+  (or (> v1 v2)
+      (and (= v1 v2)
+           (< (compare k1 k2) 0))))
 
 (defn calc-checksum [room-name]
   (->> (s/replace room-name "-" "")
        (frequencies)
-       (sort-by val)
-       (reverse)
-       (partition-by val)
-       (map #(map first %))
-       (map sort)
+       (sort compare-freq)
        (flatten)
+       (take-nth 2)
        (take 5)
        (s/join)))
 
 (defn sector-value [room]
-  (if (= (checksum room) (calc-checksum (room-name room)))
-    (Integer. (sector-id room))
-    0))
+  (let [length (count room)]
+    (letfn [(room-name [room] (subs room 0 (- length 11)))
+            (sector-id [room] (subs room (- length 10) (- length 7)))
+            (checksum [room] (subs room (- length 6) (- length 1)))]
+      (if (= (checksum room) (calc-checksum (room-name room)))
+        (Integer. (sector-id room))
+        0))))
 
 (defn rooms [path]
   (line-seq (io/reader path)))
