42b6a413cc66ffc9de4c4f24537e94d01d152c90
Allan Berger <allan@blossom.io>
2016-12-24 00:05:50 +0100

    feat(aoc): add day6 part 2, refactor part 1 and 2 into functions

--- clojure/adventofcode/src/adventofcode/days/day6.clj
+++ clojure/adventofcode/src/adventofcode/days/day6.clj
@@ -4,11 +4,25 @@
 (defn input [path]
   (line-seq (io/reader path)))
 
-(doseq [x (apply map vector (input "././files/day6_input.txt"))]
-  (->> (frequencies x)
-       (sort-by val)
-       (reverse)
-       (ffirst)
-       (prn)))
+(defn part1
+  "http://adventofcode.com/2016/day/6"
+  []
+  (doseq [x (apply map vector (input "././files/day6_input.txt"))]
+    (->> (frequencies x)
+         (sort-by val)
+         (reverse)
+         (ffirst)
+         (prn))))
 
 ; => \w \k \b \v \m \i \k \b
+
+(defn part2
+  "http://adventofcode.com/2016/day/6"
+  []
+  (doseq [x (apply map vector (input "././files/day6_input.txt"))]
+    (->> (frequencies x)
+         (sort-by val)
+         (ffirst)
+         (prn))))
+
+; => \e \v \a \k \w \a \g \a
