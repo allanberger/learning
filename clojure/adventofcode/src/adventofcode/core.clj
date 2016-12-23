40eecacf14778b77058b3d44e13d0d8f4a6cf77e
Allan Berger <allan@blossom.io>
2016-12-23 23:11:37 +0100

    feat(aoc): day6

--- clojure/adventofcode/src/adventofcode/core.clj
+++ clojure/adventofcode/src/adventofcode/core.clj
@@ -1,7 +1,11 @@
 (ns adventofcode.core
   (:gen-class))
 
-(require '[adventofcode.days.day4 :as day4])
+(require '[adventofcode.days.day1 :as day4])
+(require '[adventofcode.days.day4 :as day6])
 
 (defn -main
-  (println (day4/part1)))
+  "Run all days"
+  [& args]
+  (println (day4/part1))
+  (println (day6/part1)))
---
+++ clojure/adventofcode/src/adventofcode/days/day6.clj
@@ -0,0 +1,13 @@
+(ns adventofcode.days.day6
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn input [path]
+  (line-seq (io/reader path)))
+
+(doseq [x (apply map vector (input "././files/day6_input.txt"))]
+  (->> (frequencies x)
+       (sort-by val)
+       (reverse)
+       (ffirst)
+       (prn)))
