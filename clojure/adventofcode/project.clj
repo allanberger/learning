9930f1020edf5205440781ac6be6f823eb8218ba
Allan Berger <allan@blossom.io>
2016-12-24 00:45:06 +0100

    env(clojure): add proto-repl dependency to make atom ink work

--- clojure/adventofcode/project.clj
+++ clojure/adventofcode/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [proto-repl "0.3.1"]]
   :main ^:skip-aot adventofcode.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
