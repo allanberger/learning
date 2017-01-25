75798eb4987b85942bb0703f766e1ea512ba5dd9
Allan Berger <allan@blossom.io>
2017-01-25 13:56:42 +0100

    fix(env): replace jre by jdk to work with lemmings-gae, via @Sgoettschkes

--- provision.sh
+++ provision.sh
@@ -10,7 +10,7 @@ readonly Args="$@"
 clojure () {
     echo "Installing clojure"
 
-    apt-get install -y default-jre
+    apt-get install -y default-jdk
     wget -q -O /home/vagrant/bin/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
     chmod a+x /home/vagrant/bin/lein
     chown vagrant:vagrant /home/vagrant/bin/lein
