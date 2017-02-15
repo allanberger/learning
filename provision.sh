3d28213e1f563d98753d653a652f927fe42fc7a8
Allan Berger <allan@blossom.io>
2017-02-15 16:57:44 +0100

    feat(vagrant): install heroku, ngrok, add greeting, fix navigation to project dir

--- provision.sh
+++ provision.sh
@@ -3,17 +3,39 @@
 set -o nounset
 set -o errexit
 
-readonly Progname=$(basename "$0")
-readonly Progdir=$(readlink -m $(dirname "$0"))
-readonly Args="$@"
+readonly BasePath="/learning"
 
 clojure () {
     echo "Installing clojure"
 
-    apt-get install -y default-jdk
+    apt-get install -y default-jre
     wget -q -O /home/vagrant/bin/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
     chmod a+x /home/vagrant/bin/lein
     chown vagrant:vagrant /home/vagrant/bin/lein
+}
+
+heroku () {
+  # Install Heroku CLI
+  apt-get install software-properties-common -y # debian only
+  add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+  curl -L https://cli-assets.heroku.com/apt/release.key | apt-key add -
+  apt-get update
+  apt-get install heroku -y
+}
+
+ngrok () {
+  # Install ngrok
+  wget -q -O /home/vagrant/ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+  unzip /home/vagrant/ngrok.zip
+  mv /home/vagrant/ngrok /home/vagrant/bin/ngrok
+  chmod a+x /home/vagrant/bin/ngrok
+  chown vagrant:vagrant /home/vagrant/bin/ngrok
+  rm /home/vagrant/ngrok.zip
+
+  mkdir -p /home/vagrant/.ngrok2
+  cp -f ${BasePath}/ngrok.yml /home/vagrant/.ngrok2/
+  chmod -R a+x /home/vagrant/.ngrok2
+  chown -R vagrant:vagrant /home/vagrant/.ngrok2
 }
 
 main () {
@@ -22,20 +44,27 @@ main () {
     export DEBIAN_FRONTEND=noninteractive
 
     # Update apt cache
-    apt-get update &> /dev/null
-    apt-get autoclean &> /dev/null
-    apt-get autoremove -y &> /dev/null
+    apt-get update
+    apt-get autoclean
+    apt-get autoremove -y
 
     # Install some base software
-    apt-get install -y curl vim &> /dev/null
+    apt-get install -y curl vim unzip
 
     # Create bin dir for user vagrant
     mkdir -p /home/vagrant/bin
     chown vagrant:vagrant /home/vagrant/bin
 
     # Navigate to project directory on login
-    echo "cd /learning" >> /home/vagrant/.bashrc
+    LINE="cd ${BasePath}"
+    FILE=/home/vagrant/.bashrc
+    grep -q "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+    # Add greeting
+    echo "Hello Allan :)" > /etc/motd
 
     clojure
+    heroku
+    ngrok
 }
-main $Args
+main
